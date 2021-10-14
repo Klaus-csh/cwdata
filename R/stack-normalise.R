b1cdf4a3c4321a6c9375898779119d597169ae79
Klaus <sche0232@student.monash.edu>
2021-10-15 00:30:44 +0800

    add stack_normalise function

---
+++ R/stack-normalise.R
@@ -0,0 +1,22 @@
+#' Convert number into proportions
+#'
+#' @param x
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#'
+#' x <- c(10, 30, 40)
+#' stack_normalize(x)
+#' x <- c(75, 0, 5, 20, NA)
+#' stack_normalise(x)
+#' x <- c(NA, NA, 10)
+#' stack_normalize(x)
+
+stack_normalise <- function(x){
+  x/sum(x)
+}
+
+#' @rdname stack_normalise
+stack_normalize <- stack_normalise
